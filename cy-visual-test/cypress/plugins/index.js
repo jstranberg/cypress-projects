7344ad68648d8a0b8e8fa9b06206b6a782644da8
seijin <devstudent89@gmail.com>
2021-02-19 11:17:23 -0600

    add cucumber to cypress/plugins

--- cy-visual-test/cypress/plugins/index.js
+++ cy-visual-test/cypress/plugins/index.js
@@ -5,3 +5,9 @@ const percyHealthCheck = require('@percy/cypress/task')
 module.exports = (on, config) => {
   on("task", percyHealthCheck)
 }
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on("file:preprocessor", cucumber())
+}
--- cypress-bdd/cypress/plugins/index.js
+++ cypress-bdd/cypress/plugins/index.js
@@ -3,3 +3,4 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber())
 }
+
