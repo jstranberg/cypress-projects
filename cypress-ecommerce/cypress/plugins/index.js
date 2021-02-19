fd68cbe4ebbba630f91ae5f3e433410a635eea88
seijin <devstudent89@gmail.com>
2021-02-19 11:24:18 -0600

    add percy to cypress/plugins

--- cypress-ecommerce/cypress/plugins/index.js
+++ cypress-ecommerce/cypress/plugins/index.js
@@ -3,3 +3,10 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber())
 }
+
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+const percyHealthCheck = require('@percy/cypress/task')
+
+module.exports = (on, config) => {
+  on("task", percyHealthCheck)
+}
