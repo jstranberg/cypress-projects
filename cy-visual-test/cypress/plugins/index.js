5034e80fff038f93337ab6307036b0480c7b0b98
seijin <devstudent89@gmail.com>
2021-02-19 11:24:47 -0600

    add percy to cypress/plugins

--- cy-visual-test/cypress/plugins/index.js
+++ cy-visual-test/cypress/plugins/index.js
@@ -1,5 +1,4 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
-
 const percyHealthCheck = require('@percy/cypress/task')
 
 module.exports = (on, config) => {
--- cypress_test/cypress/plugins/index.js
+++ cypress_test/cypress/plugins/index.js
@@ -38,3 +38,9 @@ module.exports = (on, config) => {
   on("file:preprocessor", cucumber())
 }
 
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+const percyHealthCheck = require('@percy/cypress/task')
+
+module.exports = (on, config) => {
+  on("task", percyHealthCheck)
+}
